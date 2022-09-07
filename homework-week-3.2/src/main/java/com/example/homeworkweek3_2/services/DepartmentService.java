82165fff342436449bb3b2417e875198e418fd26
Đức Long <108172013+duclongdev@users.noreply.github.com>
2022-09-08 01:48:32 +0700

    Delete homework-week-3.2 directory

--- homework-week-3.2/src/main/java/com/example/homeworkweek3_2/services/DepartmentService.java
+++
@@ -1,8 +0,0 @@
-package com.example.homeworkweek3_2.services;
-
-import com.example.homeworkweek3_2.payload.dto.DepartmentDto;
-import org.springframework.http.ResponseEntity;
-
-public interface DepartmentService {
-    ResponseEntity<?> getDepartmentDto(DepartmentDto departmentDto);
-}
--- homework-week-3.2/src/main/java/com/example/homeworkweek3_2/services/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package com.example.homeworkweek3_2.services;
-
-import com.example.homeworkweek3_2.payload.dto.EmployeeDto;
-import org.springframework.http.ResponseEntity;
-
-public interface EmployeeService {
-    ResponseEntity<?> getEmployeeDto(EmployeeDto employeeDto)  ;
-}
